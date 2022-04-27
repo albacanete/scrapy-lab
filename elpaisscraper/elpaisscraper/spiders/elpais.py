69238c83ebf1ba05b4209be8282d14b58681d748
Arian Huapaya <arian.huapaya@gmail.com>
2022-04-27 21:18:09 +0200

    All task 7.1 code done

--- elpaisscraper/elpaisscraper/spiders/elpais.py
+++ elpaisscraper/elpaisscraper/spiders/elpais.py
@@ -8,7 +8,8 @@ cleanString = lambda x: '' if x is None else unidecode.unidecode(re.sub(r'\s+','
 
 class ElpaisSpider(scrapy.Spider):
     name = 'elpais'
-    allowed_domains = ['www.elpais.com']
+    # 'elpais.com' was added to this list
+    allowed_domains = ['www.elpais.com', 'elpais.com']
     start_urls = ['https://elpais.com/']
 
     def parse(self, response):
@@ -16,36 +17,57 @@ class ElpaisSpider(scrapy.Spider):
         # We get all of the sections of El Pais newspaper
         sect = response.css("section")
 
-
         for s in sect:
-            section_name = 'No section name'
+            section_name = None
 
             # Checking at the html, we found that the name of the section is stored
             # in the data-dtm-region attribute, but since this could be empty, we 
             # previously store 'No section name' as its name.
             if 'data-dtm-region' in s.attrib:
                 section_name = s.attrib['data-dtm-region']
-            
+
+            if section_name == None: continue
+
             # We get information for every article inside the section
             for article in s.css("article"):
-                    title = article.css("a::text").extract_first()
-                    url = response.url[:-1] + article.css("a::attr(href)").extract_first()
-                    summary =  article.css("p::text").extract_first()
-                    
-                    # There's a chance that the url stored previously is from a different domain
-                    # that is not El Pais, and the previous call wouldn't be as useful as it is 
-                    # supposing that the article is in the same domain. If it is in another domain,
-                    # article.css("a::attr(href)").extract_first() will contain another https url,
-                    # so we do the next check:
-                    if url.count('https') >= 2:
-                        url = article.css("a::attr(href)").extract_first()
-                    
-                    if summary == None: summary = "No summary"
+
+                title = article.css("a::text").extract_first()
+                url = response.url[:-1] + article.css("a::attr(href)").extract_first()
+                summary =  article.css("p::text").extract_first()
                 
-                    yield {
-                    'section': section_name,
-                    'appears_ulr': response.url,
-                    'title': cleanString(title),
-                    'article_url': url,
-                    'summary': cleanString(summary),
-                    }        
+                # There's a chance that the url stored previously is from a different domain
+                # that is not El Pais, and the previous call wouldn't be as useful as it is 
+                # supposing that the article is in the same domain. If it is in another domain,
+                # article.css("a::attr(href)").extract_first() will contain another https url,
+                # so we do the next check:
+                if url.count('https') >= 2:
+                    url = article.css("a::attr(href)").extract_first()
+                
+                if summary == None: summary = "No summary"
+            
+                yield {
+                'section': section_name,
+                'appears_ulr': response.url,
+                'title': cleanString(title),
+                'article_url': url,
+                'summary': cleanString(summary),
+                }
+                next_page = url
+
+                # We use the article's URL to go to the next page:
+                if next_page is not None:
+                    yield scrapy.Request(next_page, callback=self.parse_article)
+
+    def parse_article(self, response):
+
+        # And we finally extract all of tge information from the article
+        title = response.css("h1::text").extract_first()
+        authors = response.css(".a_md_a_n::text").extract_first()
+        content = ' '.join(response.css(".a_c p::text").extract())
+
+        yield {
+            'appears_ulr': response.url,
+            'title': cleanString(title),
+            'authors': cleanString(authors),
+            'contents': cleanString(content),
+        }
