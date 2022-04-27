fbcf11e1a0e6e9da98675a85f1494807c4efcbf6
Arian Huapaya <arian.huapaya@gmail.com>
2022-04-27 16:39:04 +0200

    First el pais script done

--- elpaisscraper/elpaisscraper/spiders/elpais.py
+++ elpaisscraper/elpaisscraper/spiders/elpais.py
@@ -12,44 +12,40 @@ class ElpaisSpider(scrapy.Spider):
     start_urls = ['https://elpais.com/']
 
     def parse(self, response):
-        # logging.log(logging.WARNING, response.css("section").extract())
+
+        # We get all of the sections of El Pais newspaper
         sect = response.css("section")
-        # logging.log(logging.WARNING, sect.attrib['class'])
 
-        # for secttitle in response.css("section::attr(data-dtm-region)").extract():
-        #     # sect_name = sect.attrib['class']
-        #     # if "data-dtm-region" in sect.attrib:
-            
-
-        #     logging.log(logging.WARNING, secttitle)
 
         for s in sect:
             section_name = 'No section name'
 
+            # Checking at the html, we found that the name of the section is stored
+            # in the data-dtm-region attribute, but since this could be empty, we 
+            # previously store 'No section name' as its name.
             if 'data-dtm-region' in s.attrib:
                 section_name = s.attrib['data-dtm-region']
             
-            # logging.log(logging.WARNING, section_name)
+            # We get information for every article inside the section
             for article in s.css("article"):
                     title = article.css("a::text").extract_first()
                     url = response.url[:-1] + article.css("a::attr(href)").extract_first()
                     summary =  article.css("p::text").extract_first()
                     
+                    # There's a chance that the url stored previously is from a different domain
+                    # that is not El Pais, and the previous call wouldn't be as useful as it is 
+                    # supposing that the article is in the same domain. If it is in another domain,
+                    # article.css("a::attr(href)").extract_first() will contain another https url,
+                    # so we do the next check:
                     if url.count('https') >= 2:
                         url = article.css("a::attr(href)").extract_first()
+                    
                     if summary == None: summary = "No summary"
-                # yield{
-                    # logging.log(logging.WARNING, title)
-                    # logging.log(logging.WARNING, url)
-                    # logging.log(logging.WARNING, p)
-                # }
+                
                     yield {
                     'section': section_name,
                     'appears_ulr': response.url,
                     'title': cleanString(title),
                     'article_url': url,
                     'summary': cleanString(summary),
-                    }
-        # logging.log(logging.WARNING, response.css(".c_t a::text").extract())
-
-        
+                    }        
